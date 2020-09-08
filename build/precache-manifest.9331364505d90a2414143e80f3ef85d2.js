96c2cb8c2bd5963013b842ec9dc7c91f7908c62b
Ilari Lehtinen <ilari.m.lehtinen@gmail.com>
2020-09-08 14:40:29 +0300

    added frontend production build

---
+++ build/precache-manifest.9331364505d90a2414143e80f3ef85d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4fdf566b241691b568c24ed553bb11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7472dda365d2901f4afa",
+    "url": "/static/css/main.253b5503.chunk.css"
+  },
+  {
+    "revision": "c833f9d86cf79ff4355b",
+    "url": "/static/js/2.3a567198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a567198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7472dda365d2901f4afa",
+    "url": "/static/js/main.409d7458.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
---
+++ build/precache-manifest.a0e349b8d8c4e30b310c06513a518a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e552fbe2b614e11f199d99024d4da3f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4ff368172e981d7224",
+    "url": "/static/css/main.253b5503.chunk.css"
+  },
+  {
+    "revision": "c833f9d86cf79ff4355b",
+    "url": "/static/js/2.3a567198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a567198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4ff368172e981d7224",
+    "url": "/static/js/main.384c5a2b.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
